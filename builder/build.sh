1395b539bd567b8162cbd2706d0d5b86b7a7fb5c
wass <wassim.filali@gmail.com>
2024-04-13 15:40:58 +0200

    fix copy problem by direct mapping

--- builder/build.sh
+++ builder/build.sh
@@ -3,12 +3,5 @@
 # Navigate into the astro-big-doc directory
 cd /astro-big-doc
 
-# Copy necessary files from the user_repo
-rm -rf /astro-big-doc/content
-rm -rf /astro-big-doc/public
-cp -R /user_repo/content /astro-big-doc
-cp -R /user_repo/public /astro-big-doc
-cp /user_repo/menu.yaml /astro-big-doc/menu.yaml
-
 # Build the site
 pnpm run build
